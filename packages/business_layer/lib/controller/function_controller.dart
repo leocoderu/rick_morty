d9eef0f829cc88411906dae41e1effedec2b01ba
Leocode <leo@leocode.ru>
2025-10-17 17:03:33 +0300

    Delete unused files

--- packages/business_layer/lib/controller/function_controller.dart
+++
@@ -1,8 +0,0 @@
-// Import Layers
-import 'package:data_layer/data_layer.dart';
-
-class FunctionController {
-  final hFunc1 = heavyFunction1;
-  final hFunc2 = heavyFunction2;
-  final hFunc3 = heavyFunction3;
-}
